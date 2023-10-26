e13c7f1962eb1d1a6c10efd1cea62867aa26c794
othmman00 <148593214+othmman00@users.noreply.github.com>
2023-10-26 09:22:13 +0000

    Intel setup

--- constants.py
+++ constants.py
@@ -1,3 +1,3 @@
 # Replace with your own OpenAI API Key https://platform.openai.com/account/api-keys
 # and rename this file to constants.py.
-APIKEY = "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"
+APIKEY = "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"
