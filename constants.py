879f6bc8e17c6a0968200b6d152993d2ad29a1e7
othmman00 <148593214+othmman00@users.noreply.github.com>
2023-10-26 09:38:33 +0000

    new data setup

--- constants.py
+++ constants.py
@@ -1,3 +1,3 @@
 # Replace with your own OpenAI API Key https://platform.openai.com/account/api-keys
 # and rename this file to constants.py.
-APIKEY = "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"
+APIKEY = "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"
